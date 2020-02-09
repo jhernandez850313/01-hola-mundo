3ebb352437dc0fedb7d4b871da3de8b912f28172
Joel Hernandez <jhernandez850313@gmail.com>
2020-02-08 20:27:01 -0600

    terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  nombre:string = 'Joel';
+  apellido:string = 'Hernandez';
 }
